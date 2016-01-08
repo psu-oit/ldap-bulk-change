a2ac061b07552c67e291568a226e2a17be10dfc7
Aaron DeVore <adevore@pdx.edu>
2016-01-08 09:12:51 -0800

    Refactor to a more procedural style

--- ldap-bulk-change.py
+++ ldap-bulk-change.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-__author__ = 'hhauer'
 
 import os
 import sys
@@ -7,14 +6,19 @@ import argparse
 import configparser
 import re
 
-#http://pythonhosted.org/python3-ldap/
+# http://pythonhosted.org/python3-ldap/
 from ldap3 import Connection, Server, MODIFY_REPLACE
+
+__author__ = 'hhauer'
 
 # Start by setting up argparse
 parser = argparse.ArgumentParser(description='Make a bulk change to users in LDAP.')
-parser.add_argument('--verbose', '-v', action='count', help="Set the verbosity level.")
-parser.add_argument('--nossl', action="store_true", help="Connect without SSL.")
-parser.add_argument('--environment', '-e', nargs="?", help="Use one of the environments defined in ~/.ldap_envs instead.")
+parser.add_argument('--verbose', '-v', action='count',
+                    help="Set the verbosity level.")
+parser.add_argument('--nossl', action="store_true",
+                    help="Connect without SSL.")
+parser.add_argument('--environment', '-e', nargs="?",
+                    help="Use one of the environments defined in ~/.ldap_envs instead.")
 # TODO: Add dry-run option to show what changes would be made.
 # TODO: Make verbose do something.
 
@@ -32,66 +36,90 @@ parser.add_argument('change_attr', nargs=1, help="The attribute to be changed.")
 parser.add_argument('regexp', nargs=1, help="A regexp used to determine the new value of change_attr.")
 parser.add_argument('replace', nargs=1, help="The value to substitute into the new value of change_attr.")
 
-args = parser.parse_args()
 
-# Load any environment configurations.
-config = configparser.ConfigParser()
-config.read(['.ldap_envs', os.path.expanduser('~/.ldap_envs')])
+def main():
+    args = parser.parse_args()
+    target = load_config(args)
+    connection = connect(args, target)
+    search_results = search(args, target, connection)
+    change_set = apply_regex(args, search_results)
+    commit(args, connection, change_set)
+    connection.unbind()
 
-# Build a record of the target environment.
-if args.environment is not None and args.environment in config:
-    target = config[args.environment]
-else: #TODO: Where it make sense, make command line args overwrite .ldap_envs values.
-    target = {
-        'host': args.host,
-        'port': args.port,
-        'bind_dn': args.bind_dn,
-        'password': args.password,
-        'base_dn': args.base_dn,
-    }
 
-# Make sure we have all the necessary parameters one way or another.
-soft_fail = False
-for key in target.keys():
-    if target[key] is None:
-        print("No value for parameter: " + key)
-        soft_fail = True
+def load_config(args):
+    # Load any environment configurations.
+    config = configparser.ConfigParser()
+    config.read(['.ldap_envs', os.path.expanduser('~/.ldap_envs')])
+    # Build a record of the target environment.
+    if args.environment is not None and args.environment in config:
+        target = config[args.environment]
+    else:
+        # TODO: Where it make sense, make command line args overwrite .ldap_envs values.
+        target = {
+            'host': args.host,
+            'port': args.port,
+            'bind_dn': args.bind_dn,
+            'password': args.password,
+            'base_dn': args.base_dn,
+        }
 
-# If we hit a soft fail condition die out.
-if soft_fail:
-    sys.exit(1)
+    # Make sure we have all the necessary parameters one way or another.
+    soft_fail = False
+    for key in target.keys():
+        if target[key] is None:
+            print("No value for parameter: " + key)
+            soft_fail = True
 
-# Open a connection to the LDAP server.
-if args.nossl:
-    server = Server(target['host'], port=int(target['port']))
-else:
-    server = Server(target['host'], port=int(target['port']), use_ssl=True)
+    # If we hit a soft fail condition die out.
+    if soft_fail:
+        sys.exit(1)
 
-connection = Connection(server, user=target['bind_dn'], password=target['password'], auto_bind=True)
 
-# Find our set of target DNs.
-connection.search(target['base_dn'], args.filter[0], attributes=args.change_attr)
+def connect(args, target):
+    # Open a connection to the LDAP server.
+    if args.nossl:
+        server = Server(target['host'], port=int(target['port']))
+    else:
+        server = Server(target['host'], port=int(target['port']), use_ssl=True)
 
-change_set = {}
-for record in connection.response:
-    change_set[record['dn']] = record['attributes'][args.change_attr[0]]
+    return Connection(server, user=target['bind_dn'],
+                      password=target['password'], auto_bind=True)
 
-# Compile our regular expression.
-regexp = re.compile(args.regexp[0])
 
-# Calculate the new values.
-for dn in change_set:
-    new_values = []
-    for attr in change_set[dn]:
-        new_values.append(regexp.sub(args.replace[0], attr))
+def search(args, target, connection):
+    # Find our set of target DNs.
+    connection.search(target['base_dn'], args.filter[0],
+                      attributes=args.change_attr)
 
-    change_set[dn] = {
-        args.change_attr[0]: (MODIFY_REPLACE, new_values),
-    }
+    results = {}
+    for record in connection.response:
+        results[record['dn']] = record['attributes'][args.change_attr[0]]
 
-#Set the new values in LDAP.
-for dn in change_set:
-    connection.modify(dn, change_set[dn])
-    print("Modify: {}: {}".format(dn, connection.result['description']))
+    return results
 
-connection.unbind()
+
+def apply_regex(args, search_results):
+    regexp = re.compile(args.regexp[0])
+    change_set = {}
+
+    for dn in search_results:
+        new_values = [regexp.sub(args.replace[0], attr)
+                      for attr in change_set[dn]]
+
+        change_set[dn] = {
+            args.change_attr[0]: (MODIFY_REPLACE, new_values),
+        }
+
+    return change_set
+
+
+def commit(args, connection, change_set):
+    # Set the new values in LDAP.
+    for dn in change_set:
+        connection.modify(dn, change_set[dn])
+        print("Modify: {}: {}".format(dn, connection.result['description']))
+
+
+if __name__ == "__main__":
+    main()
